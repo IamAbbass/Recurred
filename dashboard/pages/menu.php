ed9ec72682d2ec74f28180e6dbb77d91215bfdcc
danibhatti <danibhatti1993@gmail.com>
2019-08-30 18:16:09 +0500

    Updated by daniyal
    worked on dashboard

--- dashboard/pages/menu.php
+++ dashboard/pages/menu.php
@@ -33,7 +33,7 @@
 
     <li class="nav-item">
       <a class="nav-link" href="index.php">
-        <i class="fas fa-fw fa-line-chart"></i>
+      <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Reinvest Savings</span></a>
     </li>
 
@@ -52,87 +52,39 @@
     <li class="nav-item">
       <a class="nav-link" href="index.php">
         <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Reports</span></a>
-    </li>
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-      Interface
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
-      </a>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Components:</h6>
-          <a class="collapse-item" href="buttons.php">Buttons</a>
-          <a class="collapse-item" href="cards.php">Cards</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
-      </a>
-      <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Utilities:</h6>
-          <a class="collapse-item" href="utilities-color.php">Colors</a>
-          <a class="collapse-item" href="utilities-border.php">Borders</a>
-          <a class="collapse-item" href="utilities-animation.php">Animations</a>
-          <a class="collapse-item" href="utilities-other.php">Other</a>
-        </div>
-      </div>
+        <span>Saving Trackings</span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
+
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Activity Breakdown</span></a>
+    </li>
+
     <!-- Heading -->
-    <div class="sidebar-heading">
-      Addons
-    </div>
+    <hr class="sidebar-divider">
 
-    <!-- Nav Item - Pages Collapse Menu -->
+
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-        <i class="fas fa-fw fa-folder"></i>
-        <span>Pages</span>
-      </a>
-      <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Login Screens:</h6>
-          <a class="collapse-item" href="login.php">Login</a>
-          <a class="collapse-item" href="register.php">Register</a>
-          <a class="collapse-item" href="forgot-password.php">Forgot Password</a>
-          <div class="collapse-divider"></div>
-          <h6 class="collapse-header">Other Pages:</h6>
-          <a class="collapse-item" href="404.php">404 Page</a>
-          <a class="collapse-item" href="blank.php">Blank Page</a>
-        </div>
-      </div>
+      <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Profile</span></a>
     </li>
 
-    <!-- Nav Item - Charts -->
+    <!-- Heading -->
+    <hr class="sidebar-divider">
+
+
     <li class="nav-item">
-      <a class="nav-link" href="charts.php">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span>Charts</span></a>
+      <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Link Accounts</span></a>
     </li>
 
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-      <a class="nav-link" href="tables.php">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span></a>
-    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
